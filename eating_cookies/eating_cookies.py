219ece58ebde018bcd12ff3d3e564b222f5c7f21
berdiyeva <berdiyevaaltyn@gmail.com>
2020-09-14 18:37:44 -0500

    some code is here

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,27 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+import sys              #to use a sys.argv
 
-    pass
+
+# The cache parameter is here for if you want to implement
+# a solution that is more efficient than the naive 
+# recursive solution
+
+def eating_cookies(n, cache={1: 1}):
+  if n < 0:             #if n is less than zero (for all the negatives)
+      return 0
+  if n == 0:            #as cookie monster can eat 0 cookies at a time
+      return 1
+  if n not in cache:
+      cache[n] = eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+  return cache[n]
+
+  return eating_cookies(n-1) + eating_cookies(n-2)  + eating_cookies(n-3)
 
 if __name__ == "__main__":
-    # Use the main function here to test out your implementation
-    num_cookies = 5
-
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+  if len(sys.argv) > 1:  
+    num_cookies = int(sys.argv[1])
+    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+  else:
+    print('Usage: eating_cookies.py [num_cookies]')
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python
 
 import sys
+from typing import Any, List
+
+# For each player
+# there are three options
+# for every option of all other players
 
 def rock_paper_scissors(n):
   # Your code here
 
-  pass
+  def multiply_list(list1: List[Any], list2: List[Any]):
+    if not list1:
+        return list2
+
+    if not list2:
+        return list1
+
+    combined = []
+    for item1 in list1:
+        for item2 in list2:
+            combined.append([*item1, *item2])
+
+    return combined
 
 
 if __name__ == "__main__":
